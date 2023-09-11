9422c1d116cca6f482bad9c62f1c8104de8e621e
lanbow93 <the.doctor1501@gmail.com>
2023-09-11 11:33:40 -0500

    mid navigation menu

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react"
+
 function Navigation(props: any){
+    const [viewSubMenu, setViewSubMenu] = useState(false)
+
+    const handleSubMenu = () => {
+        if(viewSubMenu){
+            setViewSubMenu(false)
+        }else{
+            setViewSubMenu(true)
+        }
+    }
     return <nav>
-        <h1>Nav Bar</h1>
+        <div className="banner">
+            <div id="navLogo">
+                <p>LOGO</p>
+            </div>
+            <h1>Speedy Check-In</h1>
+            <button onClick={handleSubMenu}>≡</button>
+        </div>
+        <div className={`navOptions ${viewSubMenu ? "showMenu" : ""}`}>
+            <h1>Hidden Options</h1>
+        </div>
     </nav>
 }
 
