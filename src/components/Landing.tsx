81fdf6f823b5d016615d490770ffd3204f0ba720
lanbow93 <the.doctor1501@gmail.com>
2023-09-11 09:40:07 -0500

    basic template connected

---
+++ src/components/Landing.tsx
@@ -0,0 +1,7 @@
+function Landing(){
+    return <div className="landing">
+        <h1>Landing Page Reached</h1>
+    </div>
+}
+
+export default Landing
---
+++ src/router/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import App from "../App";
+import Landing from "../components/Landing";
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<App />} >
+        <Route path="/" element={<Landing />} />
+    </Route>
+))
+
+export default router
