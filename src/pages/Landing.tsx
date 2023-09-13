4c509ba298654f7a8da1398a183efe2a4273743c
lanbow93 <the.doctor1501@gmail.com>
2023-09-13 10:29:56 -0500

    Made nav bar darker

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,13 @@
 function Landing(){
     return <div className="landing">
-        <h1>Landing Page Reached</h1>
+    <h1>Revolutionize Workforce Management with Speedy Check-In</h1>
+
+    <p>Welcome to Speedy Check-In, your comprehensive solution for efficient workforce management!</p>
+    <p>Our web application simplifies the entire process, allowing managers to effortlessly create, enroll, assign groups, and set schedules.</p>
+    <p>With the ability to generate QR codes, managers can streamline check-in procedures.</p>
+    <p>When users scan the QR code, it not only validates their presence but also records their punch-in and out times, ensuring precise attendance tracking.</p>
+     <p>Additionally, users can conveniently access their schedules and view assigned tasks, along with the responsible manager's details. Experience the future of workforce management with Speedy Check-In today!</p>
+
     </div>
 }
 
