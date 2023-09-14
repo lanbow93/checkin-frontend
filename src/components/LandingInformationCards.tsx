30e80b1ac65c013ac2ea61e66d45f60db5075835
lanbow93 <the.doctor1501@gmail.com>
2023-09-13 22:50:07 -0500

    accordian menu functional

--- src/components/LandingInformationCards.tsx
+++ src/components/LandingInformationCards.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react"
+
 interface landingCardProps{
     cardTitle: string,
     cardContent: string
 }
+
+
 function LandingInformationCard(props: landingCardProps){
-    
-    return <div className="landingCard">
-        <h2 className="cardTitle">{props.cardTitle}</h2>
-        <p className="cardContent">{props.cardContent}</p>
+    const [isActive, setIsActive] = useState(false)
+    const handleClick = () => {
+        if(isActive){
+            setIsActive(false)
+        } else{
+            setIsActive(true)
+        }
+    }
+    return <div className={`landingCard `}  >
+        <h2 className="cardTitle" onClick={handleClick}>{props.cardTitle}</h2>
+        <p className={`cardContent ${isActive ? "cardActivated" : "cardDeactivated"}`}>{props.cardContent}</p>
     </div>
 }
 
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,10 +2,12 @@ import LandingInformationCard from "../components/LandingInformationCards"
 
 function Landing(){
     return <div className="landing">
-    <h1>Revolutionize Workforce Management with<br /> <span className="title">Speedy Check-In</span></h1>
+    <h1>Revolutionize Workforce Management with<br /><span className="title">Speedy Check-In</span></h1>
 
     <div className="landingDescription">
-        <LandingInformationCard cardTitle="" cardContent="Welcome to Speedy Check-In, your comprehensive solution for efficient workforce management!" />
+        <div className="greeting">
+            <p>Welcome to Speedy Check-In, your comprehensive solution for efficient workforce management!</p>
+        </div>
         <LandingInformationCard cardTitle="About" cardContent="Our web application simplifies the entire process, allowing managers to effortlessly create, enroll, assign groups, and set schedules."/>
         <LandingInformationCard cardTitle="How" cardContent="With the ability to generate QR codes, managers can streamline check-in procedures."/>
         <LandingInformationCard cardTitle="Breakdown" cardContent="When users scan the QR code, it not only validates their presence but also records their punch-in and out times, ensuring precise attendance tracking."/>
