73e243eb4dc60233e8d60ddeb8eb166430a27e84
lanbow93 <the.doctor1501@gmail.com>
2023-09-15 10:39:13 -0500

    form finished on mobile

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -22,26 +22,28 @@ function SignUp(){
     }
     return <div className="registration">
         
-        <form action="">
+        <form action="/register" method="POST">
             <h2>Sign Up Page</h2>
-            <p className="finePrint">* username limit 15 characters</p>
+            <p className="finePrint">* Username 15 Character Limit</p>
             <label >Username</label>
             <input type="text" name="username" value={username} onChange={(event) => event.target.value.length <= 14 ? setUserName(event.target.value) : ""  } />
             <label >Password</label>
             <input type="password" className="passwordRow" value={password} onChange={(event) => setPassword(event.target.value)}/>
             <label >Verify Password</label>
             <input type="password" name="verifypassword" value={verifypassword} onChange={(event) => setVerifyPassword(event.target.value)} />
+            <h2>Account Information</h2>
+            <p className="finePrint">* Badge Name 15 Character Limit</p>
             <label>First Name</label>
             <input type="text" name="firstname" value={firstName} onChange={(event) => {handleNameChange(event.target.value, true)}} />
             <label>Last Name</label>
             <input type="text" name="firstname" value={lastName} onChange={(event) => {handleNameChange(event.target.value, false)}} />
+            <label>Email</label>
+            <input type="email" name="email" value={email} onChange={(event) => {setEmail(event.target.value)}} />
             <label>Badge Name</label>
-            <input type="text" name="badgeName" />
+            <input type="text" name="badgeName" value={badgeName} onChange={(event) => event.target.value.length <= 14 ? setBadgeName(event.target.value) : ""}/>
             {firstName && lastName && password && password === verifypassword && username && email ? <button type="submit" className="activated" >Register</button> : <button type="submit" disabled>Register</button> }
-
             <input type="hidden" value={name} name="name" />
         </form>
-        <p>Name: {name}</p>
     </div>
 }
 
