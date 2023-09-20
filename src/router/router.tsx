7ddda0b9405e5ce0f2a4aff60ad133ffe38c67e2
lanbow93 <the.doctor1501@gmail.com>
2023-09-20 12:31:28 -0500

    forgotpassword route connected to component

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,6 +4,7 @@ import Landing from "../pages/Landing";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
 import HomePage from "../pages/HomePage";
+import ForgotPassword from "../pages/ForgotPassword";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App />} >
@@ -11,6 +12,7 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="/signup" element={<SignUp />}/>
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<HomePage/>} />
+        <Route path="/forgotpassword" element={<ForgotPassword />}/>
     </Route>
 ))
 
