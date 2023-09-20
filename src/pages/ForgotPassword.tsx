9b47f92fb66c58620a518468100b61ee33b9d3b9
lanbow93 <the.doctor1501@gmail.com>
2023-09-20 12:29:47 -0500

    made form css more uniform

---
+++ src/pages/ForgotPassword.tsx
@@ -0,0 +1,9 @@
+function ForgotPassword(){
+    return <div className="forgotPassword">
+        <form>
+            <h2>Forgot Password</h2>
+        </form>
+    </div>
+}
+
+export default ForgotPassword
