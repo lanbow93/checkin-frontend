dad3b759b99e5a703c40d6e8bdb833ef2e06d2ad
lanbow93 <the.doctor1501@gmail.com>
2023-09-27 15:01:37 -0500

    removed unnessary values

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import ErrorScreen from '../components/ErrorScreen'
 import LoadingScreen from '../components/LoadingScreen'
@@ -7,11 +7,15 @@ import url from '../router/url'
 function Login() {
     const [isLoading, setIsLoading] = useState(false)
     const [isModalActive, setIsModalActive] = useState(false)
-    const [errorMessage, setErrorMessage] = useState('')
-    const [errorStatus, setErrorStatus] = useState('')
-    const [errorAdditional, setErrorAdditional] = useState('')
-    const [username, setUserName] = useState('')
-    const [password, setPassword] = useState('')
+    const [errorData, setErrorData] = useState({
+        errorMessage: '',
+        errorStatus: '',
+        errorAdditional: '',
+    })
+    const [userData, setUserData] = useState({
+        username: '',
+        password: '',
+    })
     const navigate = useNavigate()
 
     const handleSubmission = async (
@@ -19,10 +23,6 @@ function Login() {
     ) => {
         event.preventDefault()
         setIsLoading(true)
-        const user = {
-            username: username,
-            password: password,
-        }
         try {
             const response = await fetch(url + '/user/login', {
                 method: 'POST',
@@ -30,17 +30,19 @@ function Login() {
                     'Content-Type': 'application/json',
                 },
                 credentials: 'include',
-                body: JSON.stringify(user),
+                body: JSON.stringify(userData),
             })
 
             if (response.ok) {
                 navigate('/home')
             } else {
                 const data = await response.json()
-                console.log(data)
-                setErrorStatus(data.error)
-                setErrorAdditional(data.message)
-                setErrorMessage(data.status)
+                const { error, message, status } = data
+                setErrorData({
+                    errorMessage: status,
+                    errorAdditional: message,
+                    errorStatus: error,
+                })
                 setIsModalActive(true)
             }
         } catch (error) {
@@ -50,13 +52,17 @@ function Login() {
         }
     }
 
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setUserData({ ...userData, [event.target.name]: event.target.value })
+    }
+
     return (
         <div className="loginPage">
             <div className={`errorModal ${isModalActive ? 'showError' : ''}`}>
                 <ErrorScreen
-                    message={errorMessage}
-                    status={errorStatus}
-                    error={errorAdditional}
+                    message={errorData.errorMessage}
+                    status={errorData.errorStatus}
+                    error={errorData.errorAdditional}
                     closeModal={setIsModalActive}
                 />
             </div>
@@ -68,27 +74,22 @@ function Login() {
                     <h2>Speedy Login</h2>
                     <label>Username:</label>
                     <input
+                        required
                         type="text"
                         name="username"
-                        value={username}
-                        onChange={(event) => setUserName(event.target.value)}
+                        value={userData.username}
+                        onChange={handleInputChange}
                     />
                     <label>Password:</label>
                     <input
                         type="password"
                         name="password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
+                        value={userData.password}
+                        onChange={handleInputChange}
                     />
-                    {username && password ? (
-                        <button type="submit" className="activated">
-                            Submit
-                        </button>
-                    ) : (
-                        <button type="submit" className="deactivated" disabled>
-                            Submit
-                        </button>
-                    )}
+                    <button type="submit" className="activated">
+                        Submit
+                    </button>
                     <Link to="/forgotpassword" className="forgotLink">
                         Forgot Password
                     </Link>
